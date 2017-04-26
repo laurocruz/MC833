a7023400b68c0f6ee9ed77074594e0867346d690
Lauro Cruz <laurocruzsouza@gmail.com>
2017-04-26 11:21:26 -0300

    Finished codes, Makefile made

--- projeto1/client.c
+++ projeto1/client.c
@@ -4,25 +4,26 @@
 #include <strings.h>
 #include <unistd.h>
 #include <netdb.h>
+#include <errno.h>
 
 #define SERVER_PORT 12345
 #define MAX_LINE 256
 
 int main(int argc, char * argv[]) {
-        struct hostent *host_address;
-        struct sockaddr_in socket_address;
-        char *host;
-        char buf[MAX_LINE];
-        int s;
-        int len;
-        int port;
+    struct hostent *host_address;
+    struct sockaddr_in socket_address;
+    char *host;
+    char buf[MAX_LINE];
+    int s;
+    int len;
+    int port;
 
 
-	/* verificaÃ§Ã£o de argumentos */
+    /* verificaÃ§Ã£o de argumentos */
     if (argc != 2 && argc != 3)  {
-        printf("ERROR: Wrong number of arguments\n");
-        printf("%s hostname [PORT]\n", argv[0]);
-        exit(1);
+        perror("ERROR: Wrong number of arguments\n");
+        printf("Usage: %s hostname [PORT]\n", argv[0]);
+        exit(errno);
     } else if (argc == 3) {
         port = atoi(argv[2]);
     } else port = SERVER_PORT;
@@ -33,41 +34,44 @@ int main(int argc, char * argv[]) {
     host_address = gethostbyname(host);
 
     if (!host_address) {
-        printf("ERROR: Unable to resolve hostname\n");
-        exit(1);
+        perror("ERROR: Unable to resolve hostname\n");
+        exit(errno);
     }
 
     /* criaÃ§Ã£o de socket ativo*/
-    
-    if ((s = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        printf("ERROR: Unable to create socket\n");
-        exit(1);
-    }
 
-    /* criaÃ§Ã£o da estrutura de dados de endereÃ§o */
-    bzero((char *)&socket_address, sizeof(socket_address));
-    
-    socket_address.sin_family = AF_INET;
-    socket_address.sin_addr = *(struct in_addr *)host_address->h_addr_list[0];
-    socket_address.sin_port = htons(port);
+        if ((s = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+            perror("ERROR: Unable to create socket\n");
+            exit(errno);
+        }
+
+        /* criaÃ§Ã£o da estrutura de dados de endereÃ§o */
+        bzero((char *)&socket_address, sizeof(socket_address));
+
+        socket_address.sin_family = AF_INET;
+        socket_address.sin_addr = *(struct in_addr *)host_address->h_addr_list[0];
+        socket_address.sin_port = htons(port);
 
 
-	/* estabelecimento da conexÃ£o */
-    if (connect(s, (struct sockaddr *) &socket_address, sizeof(socket_address)) == -1) {
-        printf("ERROR: Unable to connect to server\n");
-        exit(1);
-    }
+        /* estabelecimento da conexÃ£o */
+        if (connect(s, (struct sockaddr *) &socket_address, sizeof(socket_address)) == -1) {
+            perror("ERROR: Unable to connect to server\n");
+            exit(errno);
+        }
 
-    /* ler e enviar linhas de texto, receber eco */
+        /* ler e enviar linhas de texto, receber eco */
 
-    /* Envia texto */
-    memset(&buf, '\0', MAX_LINE);
-    sprintf(buf, "TESTE_cli\n");
-    send(s, buf, MAX_LINE, 0);
+        /* Envia texto */
+        memset(&buf, '\0', MAX_LINE);
+        fgets(buf, MAX_LINE, stdin);
+        //sprintf(buf, "TESTE_%d", i);
+        send(s, buf, MAX_LINE, 0);
 
-    /* Recebe eco */
-    recv(s, buf, MAX_LINE, 0);
-    printf("%s\n", buf);
+        /* Recebe eco */
+        recv(s, buf, MAX_LINE, 0);
+        printf("%s\n", buf);
+
+        close(s);
 
     return 0;
 }
--- projeto1/server.c
+++ projeto1/server.c
@@ -4,9 +4,10 @@
 #include <strings.h>
 #include <unistd.h>
 #include <netdb.h>
+#include <errno.h>
 
 #define LISTEN_PORT 12345
-#define MAX_PENDING 5
+#define MAX_PENDING 5 
 #define MAX_LINE 256
 
 int main(int argc, char * argv[]) {
@@ -22,15 +23,15 @@ int main(int argc, char * argv[]) {
 
     /* criaÃ§Ã£o da estrutura de dados de endereÃ§o */
     bzero((char *)&socket_address, sizeof(socket_address));
-    
+
     socket_address.sin_family = AF_INET;
     socket_address.sin_addr.s_addr = htonl(INADDR_ANY);
     socket_address.sin_port = htons(l_port);
-    
+
     /* criaÃ§Ã£o de socket passivo */
     if ((s = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
-        printf("ERROR: Unable to create socket\n");
-        exit(1);
+        perror("ERROR: Unable to create socket\n");
+        exit(errno);
     }
 
     /* Associar socket ao descritor */
@@ -38,22 +39,29 @@ int main(int argc, char * argv[]) {
 
     /* Criar escuta do socket para aceitar conexÃµes */
     if (listen(s, MAX_PENDING)) {
-        printf("ERROR: Unable to invoke listen method \n");
-        exit(1);
+        perror("ERROR: Unable to invoke listen method \n");
+        exit(errno);
     }
 
-    /* aguardar/aceita conexÃ£o, receber e imprimir texto na tela, enviar eco */
-    if ((new_s = accept(s, (struct sockaddr *)NULL, NULL)) == -1) {
-        printf("ERROR: Unable to get client socket\n");
-        exit(1);
+    while (1) {
+        memset(buf, '\0', MAX_LINE);
+
+        /* aguardar/aceita conexÃ£o, receber e imprimir texto na tela, enviar eco */
+        if ((new_s = accept(s, (struct sockaddr *)NULL, NULL)) == -1) {
+            perror("ERROR: Unable to get client socket\n");
+            exit(errno);
+        }
+
+        recv(new_s, buf, MAX_LINE, 0);
+        printf("%s\n", buf);
+
+        //sprintf(buf, "TESTE_ser\n"); 
+        send(new_s, buf, MAX_LINE, 0);
+
+        close (new_s);
     }
 
-    memset(buf, '\0', MAX_LINE);
-    recv(new_s, buf, MAX_LINE, 0);
-    printf("%s\n", buf);
-    
-    sprintf(buf, "TESTE_ser\n"); 
-    send(new_s, buf, MAX_LINE, 0);
+    close(s);
 
     return 0;
 }
